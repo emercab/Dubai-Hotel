50f57d8e2c6da1ba5751c33292390658f800d143
nicoAbril <abrilcruznicolas@gmail.com>
2022-08-02 00:06:10 -0500

    Se rutea el template de contacto y se realiza metodos post de contacto

--- controllers/controller_contacto.py
+++ controllers/controller_contacto.py
@@ -1,4 +1,14 @@
 # Acá van todas las clases y métodos que controlarán y harán la lógica
 # de las operaciones sobre las rutas de Contacto. Desde acá se enviará las
 # respuestas a las rutas
+import models.model_contacto as model
 
+
+def send_contacto(name,email,contact):
+    data = model.send_data_contact(name,email,contact)
+
+    if data != None:
+        print('Solicitud de contacto creada')
+    else:
+        print('Hubo un error')
+    return data
