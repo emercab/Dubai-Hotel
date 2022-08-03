8541f0e9a5bba9209b1bf1dccab08bb6a4db966e
Emerson Cabrera Salas <emercab@gmail.com>
2022-08-02 22:20:32 -0500

    Ajustes a la base de datos

--- models/model_micuenta.py
+++ models/model_micuenta.py
@@ -35,3 +35,37 @@ def get_data_login(login_field):
     return result
 # Fin de get_data_login()
 
+
+def ya_existe(valor, campo, tabla):
+    # Verifica si el valor que recibe ya existe en la tabla,
+    # retorna True en ese caso o False al contrario
+
+    try:
+        # Me conecto a la DB
+        conn = conectar()
+        # Creo el cursor que me permitirá operar en la DB
+        cursor = conn.cursor()
+        # Creo la sentencia SQL
+        sentence = f"""
+            SELECT * FROM {tabla} WHERE {campo}={valor}
+        """
+        # Ejecuto la sentencia SQL
+        cursor.execute(sentence)
+        # Guardo el primer resultado de la consulta
+        result = cursor.fetchone()
+    except Exception as error:
+        # Si hay un error, lo imprimo y retorno True indicando
+        # que no se pudo hacer la revisión
+        print(f"Error: {error}")
+        return True
+    finally:
+        # Pase lo que pase, cierro la conexión
+        conn.close()
+    
+    if len(result) == 0:
+        return False
+    else:
+        return True
+# Fin de ya_existe()
+
+
